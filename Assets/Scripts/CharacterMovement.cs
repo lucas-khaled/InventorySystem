9d711a52a26fa2ad9d36d2d4d8e6f071ffed9b92
lucas-khaled <khaledbrugger3@gmail.com>
2025-05-29 18:26:56 -0300

    Create character movement and camera settings

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed = 100f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector3 moveDirection;
+
+    public void OnMove(InputValue input) 
+    {
+        moveInput = input.Get<Vector2>();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+
+        if (moveDirection.magnitude > 0)
+            UpdateRotation();
+    }
+
+    private void UpdateRotation()
+    {
+        var rot = Quaternion.LookRotation(moveDirection, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotationSpeed * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        var movePos = transform.position + moveDirection * moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(movePos);
+    }
+}
