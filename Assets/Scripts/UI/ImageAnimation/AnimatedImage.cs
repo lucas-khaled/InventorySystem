16a886cef2b996f9108f14edd31843237b836692
lucas-khaled <khaledbrugger3@gmail.com>
2025-05-30 11:59:45 -0300

    implement activation handling

--- Assets/Scripts/UI/ImageAnimation/AnimatedImage.cs
+++ Assets/Scripts/UI/ImageAnimation/AnimatedImage.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +9,34 @@ public class AnimatedImage : MonoBehaviour
     [SerializeField]
     [AddSubclass(typeof(ImageAnimationHandler))]
     private ImageAnimationHandler handler;
+    [SerializeField]
+    private bool handleActivation;
     
     private Image image;
-    private Sequence sequence;
-    
-    private bool stopping = false;
-
+   
     private void Awake()
     {
         image = GetComponent<Image>();
+        if (handleActivation)
+            image.enabled = false;
+    }
+
+    private void Start()
+    {
+        handler.OnStart += OnStart;
+        handler.OnEnd += OnEnd;
+    }
+
+    private void OnEnd()
+    {
+        if (handleActivation)
+            image.enabled = false;
+    }
+
+    private void OnStart()
+    {
+        if (handleActivation)
+            image.enabled = true;
     }
 
     public void Play() 
